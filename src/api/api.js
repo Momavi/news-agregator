4b3f7bf8c9294de70e0165a809fd0546609430f0
Momavi <68790545+Momavi@users.noreply.github.com>
2021-08-30 00:27:52 +0300

    Added api

---
+++ src/api/api.js
@@ -0,0 +1,12 @@
+import * as axios from 'axios';
+
+const instance = axios.create({
+  baseURL: 'https://newsapi.org/v2/',
+});
+
+export const newsAPI = {
+  getNews(sortText = 'javascript', page = 0, maxResults = 30) {
+    const keyAPI = 'e17f513c62084bc18600e4a9d65c2abc'
+    return instance.get(`everything?q=${sortText.text}&sortBy=${sortText.sort}&page=${page}&pageSize=${maxResults}&apiKey=${keyAPI}`)
+  }
+}
