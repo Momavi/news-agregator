5ec8f72ad6eeb73eafe534f11bc8fd0e305ac726
Momavi <68790545+Momavi@users.noreply.github.com>
2021-08-30 00:26:45 +0300

    Added selector to 'news'

---
+++ src/redux/news-selector.js
@@ -0,0 +1,23 @@
+export const getNewsState = (state) => {
+  return state.newsPage.news;
+}
+
+export const getPageSize = (state) => {
+  return state.newsPage.pageSize;
+}
+
+export const getTotalNewsCount = (state) => {
+  return state.newsPage.totalNews;
+}
+
+export const getCurrentPage = (state) => {
+  return state.newsPage.currentPage;
+}
+
+export const getIsFetching = (state) => {
+  return state.newsPage.isFetching;
+}
+
+export const getFormData = (state) => {
+  return state.searchPage.formData;
+}
